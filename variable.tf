aa74b0270030cad13dc15188a6070136b32ec481
Subhranil Ghosh <subhranil.ghosh@tmdc.io>
2022-11-16 15:17:53 +0530

    terraform with circleci

---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  default = "us-east-2"
+}
+
+variable "profile" {
+  default = "vscode"
+}
+
+variable "key_pair" {
+  default = "terraform-key"
+}
+
+variable "backend" {
+  default = "circleci-terraform"
+}
